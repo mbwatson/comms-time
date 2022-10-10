a6a0b5ee0d40a000b21e9f9a363019c75117ef09
Matt Watson <mwatson@renci.org>
2022-10-10 14:10:19 -0400

    add dummy content to reports view

--- src/views/reports.js
+++ src/views/reports.js
@@ -1,10 +1,13 @@
-import { Fragment } from 'react'
+import { Card, CardContent, CardHeader } from '@mui/material'
 
 export const ReportsView = () => {
 
   return (
-    <Fragment>
-      rEPOrtS
-    </Fragment>
+    <Card>
+      <CardHeader title="Reports" />
+      <CardContent>
+        Lorem ipsum deserunt anim aute ut nostrud cupidatat dolore veniam aliqua ut ullamco non culpa.
+      </CardContent>
+    </Card>
   )
 }
