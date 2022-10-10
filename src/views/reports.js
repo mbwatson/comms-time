6d9d3edd3170d6531405e4c3ec76249720d610d2
Matt Watson <mwatson@renci.org>
2022-10-10 14:37:45 -0400

    add one stat

--- src/views/reports.js
+++ src/views/reports.js
@@ -1,12 +1,59 @@
-import { Card, CardContent, CardHeader } from '@mui/material'
+import PropTypes from 'prop-types'
+import { Card, CardContent, CardHeader, Divider, List, ListItem, ListItemText } from '@mui/material'
+import { useTimer } from '../context'
+
+const Statistic = ({ label, value }) => {
+  return (
+    <ListItem sx={{
+      '.MuiListItem-root': {},
+      '.MuiListItemText-root': {
+        display: 'flex',
+        alignItems: 'center',
+        gap: 2,
+      },
+      '.MuiListItemText-primary': {},
+      '.MuiListItemText-primary::after': { content: '":"' },
+      '.MuiListItemText-secondary': {},
+    }}>
+      <ListItemText
+        primary={ label }
+        secondary={ value }
+      />
+    </ListItem>
+  )
+}
+
+Statistic.propTypes = {
+  label: PropTypes.string.isRequired,
+  value: PropTypes.oneOf([
+    PropTypes.number,
+    PropTypes.string,
+  ]).isRequired,
+}
+
+//
 
 export const ReportsView = () => {
+  const { records } = useTimer()
+
+  const stats = [
+    { 'label': 'Number of records', value: records.length }
+  ]
 
   return (
     <Card>
       <CardHeader title="Reports" />
+
+      <Divider />
+
       <CardContent>
-        Lorem ipsum deserunt anim aute ut nostrud cupidatat dolore veniam aliqua ut ullamco non culpa.
+        <List>
+          {
+            stats.map((stat, i) => (
+              <Statistic key={ `report-stat-${ i }` } { ...stat } />
+            ))
+          }
+        </List>
       </CardContent>
     </Card>
   )
