6582b46ff6bbd360de545ec5ae2641f9bdf30e30
Matt Watson <mwatson@renci.org>
2022-10-13 01:09:52 -0400

    tweak file structure

--- src/data/groups.js
+++ src/data/groups.js
@@ -1,71 +1,16 @@
 export const groups = [
   /* groups */
-  {
-    id: 'nrig',
-    name: 'Network Research & Infrastructure',
-    shortName: 'NRIG',
-    color: '#e39862',
-  },
-  {
-    id: 'eds',
-    name: 'Earth Data Science',
-    shortName: 'EDS',
-    color: '#9edf7a',
-  },
-  {
-    id: 'analytics',
-    name: 'Data Science & Analytics',
-    shortName: 'Analytics',
-    color: '#1a73e8',
-  },
-  {
-    id: 'hi',
-    name: 'Health Informatics',
-    shortName: 'Health Informatics',
-    color: '#1bd6d9',
-  },
-  {
-    id: 'sa',
-    name: 'Software Architecture',
-    shortName: 'Software Architecture',
-    color: '#666',
-  },
+  { id: 'nrig',               name: 'Network Research & Infrastructure',      shortName: 'NRIG',                   color: '#e39862', },
+  { id: 'eds',                name: 'Earth Data Science',                     shortName: 'EDS',                    color: '#9edf7a', },
+  { id: 'analytics',          name: 'Data Science & Analytics',               shortName: 'Analytics',              color: '#1a73e8', },
+  { id: 'hi',                 name: 'Health Informatics',                     shortName: 'Health Informatics',     color: '#1bd6d9', },
+  { id: 'sa',                 name: 'Software Architecture',                  shortName: 'Software Architecture',  color: '#666', },
   /* collaborations */
-  {
-    id: 'bdc3',
-    name: 'BioData Catalyst coordination Center',
-    shortName: 'BDC3',
-    color: '#b33243',
-  },
-  {
-    id: 'heal',
-    name: 'HEAL Data Stewards',
-    shortName: 'HEAL',
-    color: '#441d4f',
-  },
-  {
-    id: 'irods-consortium',
-    name: 'iRODS Consortium',
-    shortName: 'iRODS Consortiuum',
-    color: '#0E8F8B',
-  },
-  {
-    id: 'ncds',
-    name: 'National Consortium for Data Science',
-    shortName: 'NCDS',
-    color: '#de2a35',
-  },
-  {
-    id: 'radx',
-    name: 'RADx DataHub',
-    shortName: 'RADx',
-    color: '#387982',
-  },
+  { id: 'bdc3',               name: 'BioData Catalyst coordination Center',   shortName: 'BDC3',                   color: '#b33243', },
+  { id: 'heal',               name: 'HEAL Data Stewards',                     shortName: 'HEAL',                   color: '#441d4f', },
+  { id: 'irods-consortium',   name: 'iRODS Consortium',                       shortName: 'iRODS Consortiuum',      color: '#0E8F8B', },
+  { id: 'ncds',               name: 'National Consortium for Data Science',   shortName: 'NCDS',                   color: '#de2a35', },
+  { id: 'radx',               name: 'RADx DataHub',                           shortName: 'RADx',                   color: '#387982', },
   /* miscellaneous bucket */
-  {
-    id: 'general',
-    name: 'General',
-    shortName: 'General',
-    color: 'coral',
-  },
+  { id: 'general',            name: 'General',                                shortName: 'General',                color: 'coral', },
 ]
