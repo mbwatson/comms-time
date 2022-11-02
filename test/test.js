1f4ef1158247d75d89fbbec1a8cd872a4c11b069
Matt Watson <mwatson@renci.org>
2022-11-01 23:47:53 -0400

    add id-uniqueness tests

--- test/test.js
+++ test/test.js
@@ -1,14 +1,35 @@
 const assert = require('assert')
 const { categories, groups, projects } = require('../src/data')
-  
+
 describe('Projects', () => {
-  it('should be non-empty', () => projects.length > 0)
+  it('should be non-empty', () => {
+    assert.notEqual(projects.length, 0)
+  })
+
+  it('should should have unique IDs', () => {
+    const ids = new Set(projects.map(p => p.id))
+    assert.equal(projects.length, ids.size)
+  })
 })
 
 describe('Groups', () => {
-  it('should be non-empty', () => groups.length > 0)
+  it('should be non-empty', () => {
+    assert.notEqual(groups.length, 0)
+  })
+
+  it('should should have unique IDs', () => {
+    const ids = new Set(groups.map(g => g.id))
+    assert.equal(groups.length, ids.size)
+  })
 })
 
 describe('Categories', () => {
-  it('should be non-empty', () => categories.length > 0)
+  it('should be non-empty', () => {
+    assert.notEqual(categories.length, 0)
+  })
+
+  it('should should have unique IDs', () => {
+    const ids = new Set(categories.map(c => c.id))
+    assert.equal(categories.length, ids.size)
+  })
 })
